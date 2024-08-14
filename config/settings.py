57a3da44b538711adb3b93f1d9f664224fdd888d
Ale1188 <dramoizta@gmail.com>
2024-08-14 07:05:09 -0700

    112 - C4

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'pages',
     'posts',
-    # 'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+from django.urls import reverse_lazy
 
-# Create your views here.
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "status", "body"]
+    success_url = reverse_lazy("list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
